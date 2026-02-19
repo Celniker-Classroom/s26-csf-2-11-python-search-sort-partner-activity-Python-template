f102c68706e83fd5fd5d057fc0adcc9fe06edca7
mackc2317 <mackc2317@my.sduhsd.net>
2026-02-19 17:45:14 +0000

    part2

--- part2.py
+++ part2.py
@@ -1,16 +1,31 @@
-from random import randint 
-ranNum_list = [] 
-# It is better to use a different variable name for the list than the loop counter 
-# Generates a list of 5 random integers between 1 and 50 inclusive. 
-for _ in range(10): # The underscore indicates that the loop counter variable is not used 
-    ranNum_list.append(randint(1,50)) # Append to the list, not the loop counter 
+from random import randint
 
-print(ranNum_list)
+# 1. Generate a list of 10 random integers between 1 and 50 inclusive.
+ranNum_list = []
+for _ in range(10):
+    ranNum_list.append(randint(1,50))
 
-# Define the number you want to find (e.g., 30, or get user input)
-num_to_find = 30 # You can change this number
+print("Generated List:", ranNum_list)
 
-if **num_to_find** in **ranNum_list**:
-    print("Number", **num_to_find**, "found in the list!")
+# 2. Define the number you want to find
+num_to_find = 30
+print("Searching for:", num_to_find)
+
+# 3. Initialize counter and tracking variable
+comparisons = 0
+found = False
+
+# 4. Perform linear search with comparison counter
+for num in ranNum_list: # Changed '_________' to 'num'
+    comparisons += 1 # Increment for each comparison
+    
+    if num == num_to_find: # Changed '_________' to 'num'
+        found = True
+        break
+
+# 5. Output results
+print("Comparisons made:", comparisons)
+if found:
+    print("Number", num_to_find, "found in the list!")
 else:
-    print("Number", **num_to_find**, "not found in the list.")
+    print("Number", num_to_find, "not found in the list.")
